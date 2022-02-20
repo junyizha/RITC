0875df836c0c004b1e4a496d0e8cb9d140445857
Junyi Zhang <junyizha@andrew.cmu.edu>
2022-02-20 14:42:36 -0500

    update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,33 +4,37 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import org.json.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        try {
+        getTick();
+    }
 
-            URL url = new URL("http://localhost:9999/v1");//your url i.e fetch data from .
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+    public static int getTick() {
+        try {
+            URL tickurl = new URL("http://localhost:9999/v1/case");
+            HttpURLConnection conn = (HttpURLConnection) tickurl.openConnection();
             conn.setRequestMethod("GET");
-            conn.setRequestProperty("Accept", "application/json");
             String apiKey = "KN0P68A7";
-            conn.setRequestProperty("x-apikey", apiKey);
+            conn.setRequestProperty("X-API-Key", apiKey);
             if (conn.getResponseCode() != 200) {
-                throw new RuntimeException("Failed : HTTP Error code : "
-                        + conn.getResponseCode());
+                throw new RuntimeException("Failed : HTTP Error code : " + conn.getResponseCode());
             }
             InputStreamReader in = new InputStreamReader(conn.getInputStream());
             BufferedReader br = new BufferedReader(in);
             String output;
             while ((output = br.readLine()) != null) {
+                JSONObject obj = new JSONObject(output);
                 System.out.println(output);
+                System.out.println(obj);
             }
             conn.disconnect();
-
         } catch (Exception e) {
             System.out.println("Exception in NetClientGet:- " + e);
         }
+        return 0;
     }
 
 }
